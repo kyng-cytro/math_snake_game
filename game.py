0ad5e899233b7ea9e0f50d4589b52448b65ff515
Kyng Cytro <johndibashi2001@gamil.com>
2024-01-04 12:09:11 +0100

    chore: background

--- game.py
+++ game.py
@@ -1,15 +1,32 @@
+import os
 import pygame
 
 pygame.init()
 
 WIDTH = 1500
 HEIGHT = 900
-GAME_SPEED = 90
+GAME_SPEED = 15
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Math Snake Game")
 
 fps = pygame.time.Clock()
+
+bg = pygame.transform.scale(pygame.image.load(
+    os.path.join('assets', 'background.jpg')), (WIDTH, HEIGHT))
+
+
+def change_snake_direction(change_to, direction):
+    if change_to == 'up' and direction != 'down':
+        return 'up'
+    if change_to == 'down' and direction != 'up':
+        return 'down'
+    if change_to == 'left' and direction != 'right':
+        return 'left'
+    if change_to == 'right' and direction != 'left':
+        return 'right'
+
+    return direction
 
 
 def main():
@@ -39,14 +56,7 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     change_to = 'right'
 
-        if change_to == 'up' and direction != 'down':
-            direction = 'up'
-        if change_to == 'down' and direction != 'up':
-            direction = 'down'
-        if change_to == 'left' and direction != 'right':
-            direction = 'left'
-        if change_to == 'right' and direction != 'left':
-            direction = 'right'
+        direction = change_snake_direction(change_to, direction)
 
         if direction == 'up':
             snake_position[1] -= 10
@@ -57,7 +67,7 @@ def main():
         if direction == 'right':
             snake_position[0] += 10
 
-        screen.fill("black")
+        screen.blit(bg, (0, 0))
 
         snake_body.insert(0, list(snake_position))
 
