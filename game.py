fa592827c33b428987739393e5d49ebdd8e72f50
kyng-cytro <johndibashi2001@gmail.com>
2024-01-10 11:53:17 +0100

    chore(ui): snake now looks better + refactored code

--- game.py
+++ game.py
@@ -1,51 +1,72 @@
-import os
 import pygame
 
 pygame.init()
 
+# constants
 WIDTH = 1500
 HEIGHT = 900
 GAME_SPEED = 15
+SNAKE_CHUCK = 10
+SNAKE_GAP = 3
+SNAKE_SIZE = 20
+SNAKE_COLOR = "green"
 
+# init
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Math Snake Game")
 
 fps = pygame.time.Clock()
 
-bg = pygame.transform.scale(pygame.image.load(
-    os.path.join('assets', 'background.jpg')), (WIDTH, HEIGHT))
+
+# globals
+direction = "right"
+change_to = direction
+snake_position = [100, 50]
+snake_body = [[100, 50],
+              [90, 50],
+              [80, 50],
+              [70, 50],
+              [60, 50],
+              [50, 50]
+              ]
 
 
-def change_snake_direction(change_to, direction):
+# changes snakes direction
+def change_snake_direction():
+    global direction
+    global change_to
     if change_to == 'up' and direction != 'down':
-        return 'up'
+        direction = 'up'
     if change_to == 'down' and direction != 'up':
-        return 'down'
+        direction = 'down'
     if change_to == 'left' and direction != 'right':
-        return 'left'
+        direction = 'left'
     if change_to == 'right' and direction != 'left':
-        return 'right'
+        direction = 'right'
 
-    return direction
+
+def modify_snake_pos():
+    if direction == 'up':
+        snake_position[1] -= SNAKE_CHUCK
+    if direction == 'down':
+        snake_position[1] += SNAKE_CHUCK
+    if direction == 'left':
+        snake_position[0] -= SNAKE_CHUCK
+    if direction == 'right':
+        snake_position[0] += SNAKE_CHUCK
 
 
 def main():
+    global change_to
+    global direction
+    global snake_position
+    global snake_body
+
     running = True
-
-    snake_position = [100, 50]
-
-    snake_body = [[100, 50],
-                  [90, 50],
-                  [80, 50],
-                  [70, 50]
-                  ]
-    direction = "right"
-    change_to = direction
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
-                pygame.quit()
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_UP:
                     change_to = 'up'
@@ -56,26 +77,21 @@ def main():
                 if event.key == pygame.K_RIGHT:
                     change_to = 'right'
 
-        direction = change_snake_direction(change_to, direction)
+        change_snake_direction()
 
-        if direction == 'up':
-            snake_position[1] -= 10
-        if direction == 'down':
-            snake_position[1] += 10
-        if direction == 'left':
-            snake_position[0] -= 10
-        if direction == 'right':
-            snake_position[0] += 10
+        modify_snake_pos()
 
-        screen.blit(bg, (0, 0))
+        screen.fill("black")
 
+        # add snake length
         snake_body.insert(0, list(snake_position))
 
+        # remove snake length
         snake_body.pop()
 
         for pos in snake_body:
-            pygame.draw.rect(screen, "green",
-                             pygame.Rect(pos[0], pos[1], 10, 10))
+            pygame.draw.rect(screen, SNAKE_COLOR,
+                             pygame.Rect(pos[0] * SNAKE_GAP, pos[1] * SNAKE_GAP, SNAKE_SIZE, SNAKE_SIZE))
 
         pygame.display.update()
         fps.tick(GAME_SPEED)
