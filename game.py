af1bc1b512e3a7877358205bfac82a39af3759f6
Kyng Cytro <johndibashi2001@gamil.com>
2024-01-24 16:50:25 +0100

    chore: cleaned up code + ui

--- game.py
+++ game.py
@@ -5,7 +5,7 @@ from random import randint
 
 pygame.init()
 
-# constants
+# Constants
 WIDTH = 1500
 HEIGHT = 900
 GAME_SPEED = 10
@@ -14,28 +14,23 @@ SNAKE_GAP = 3
 SNAKE_SIZE = 20
 SNAKE_COLOR = "green"
 
-# init
+# Initialization
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Math Snake Game")
-
 fps = pygame.time.Clock()
 
-# globals
+# Globals
 direction = "right"
 change_to = direction
+current_level = 0
+current_question = ""
 snake_position = [100, 50]
-snake_body = [[100, 50],
-              [90, 50],
-              [80, 50],
-              [70, 50],
-              ]
-
+snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
 food_positions = []
-
-# changes snakes direction
 
 
 def change_snake_direction():
+    """Changes snake direction based on user input."""
     global direction
     global change_to
     if change_to == 'up' and direction != 'down':
@@ -49,50 +44,139 @@ def change_snake_direction():
 
 
 def modify_snake_pos():
+    """Modifies snake position based on its direction."""
     if direction == 'up':
         snake_position[1] -= SNAKE_CHUCK
-    if direction == 'down':
+    elif direction == 'down':
         snake_position[1] += SNAKE_CHUCK
-    if direction == 'left':
+    elif direction == 'left':
         snake_position[0] -= SNAKE_CHUCK
-    if direction == 'right':
+    elif direction == 'right':
         snake_position[0] += SNAKE_CHUCK
 
 
-def boundary_check():
-    if snake_position[0] < 0 or snake_position[0] * SNAKE_GAP > WIDTH-10:
-        game_over()
+def draw_snake():
+    """Draws the snake on the screen."""
+    for index, pos in enumerate(snake_body):
+        if index == 0:
+            pygame.draw.circle(screen, "red", (pos[0] * SNAKE_GAP + SNAKE_SIZE // 2,
+                                               pos[1] * SNAKE_GAP + SNAKE_SIZE // 2), SNAKE_SIZE // 2)
+        else:
+            pygame.draw.rect(screen, SNAKE_COLOR,
+                             pygame.Rect(pos[0] * SNAKE_GAP, pos[1] * SNAKE_GAP, SNAKE_SIZE, SNAKE_SIZE))
 
-    if snake_position[1] < 0 or snake_position[1] * SNAKE_GAP > HEIGHT-10:
+
+def draw_question():
+    """Draws the current math question in a box with rounded corners and a calculator icon on the screen."""
+    box_width = WIDTH // 9
+    box_height = 70
+    box_margin = 20
+    border_radius = 15
+
+    box_rect = pygame.Rect(box_margin, box_margin, box_width, box_height)
+    pygame.draw.rect(screen, "darkgrey", box_rect, border_radius=border_radius)
+
+    # Load the calculator icon
+    calculator_icon = pygame.image.load("assets/calculator.svg")
+    calculator_icon = pygame.transform.scale(
+        calculator_icon, (40, 40))  # Adjust the size as needed
+
+    # Position the calculator icon
+    icon_rect = calculator_icon.get_rect(
+        topleft=(box_margin + 10, box_margin + 15))
+    screen.blit(calculator_icon, icon_rect)
+
+    question_text = pygame.font.SysFont(
+        "roboto Mono", 50).render(current_question, True, "white")
+    question_text_rect = question_text.get_rect(
+        topleft=(icon_rect.right + 10, box_margin + 18))   # Adjust vertical position
+
+    screen.blit(question_text, question_text_rect)
+
+
+def draw_food():
+    """Draws the food items on the screen."""
+    for food in food_positions:
+        answer_object = pygame.draw.rect(
+            screen, "purple", pygame.Rect(food[0], food[1], 30, 30))
+        answer_text = pygame.font.SysFont(
+            "times new roman", 20).render(f"{food[2]}", True, "black")
+        answer_text_object = answer_text.get_rect(center=answer_object.center)
+        screen.blit(answer_text, answer_text_object)
+
+
+def game_over():
+    """Displays the game over screen and exits the game."""
+    font = pygame.font.SysFont('times new roman', 50)
+    game_over_screen = font.render("GAME OVER", True, "red")
+    game_over_rect = game_over_screen.get_rect(
+        midtop=(WIDTH // 2, HEIGHT // 2))
+    screen.blit(game_over_screen, game_over_rect)
+    pygame.display.flip()
+    time.sleep(3)
+    exit_game()
+
+
+def boundary_check():
+    """Checks if the snake has hit the game boundaries."""
+    if snake_position[0] < 0 or snake_position[0] * SNAKE_GAP > WIDTH - 10 or \
+            snake_position[1] < 0 or snake_position[1] * SNAKE_GAP > HEIGHT - 10:
         game_over()
 
 
 def snake_body_check():
+    """Checks if the snake has collided with itself."""
     for block in snake_body[1:]:
         if snake_position[0] == block[0] and snake_position[1] == block[1]:
             game_over()
 
 
-def game_over():
-    # creating font object my_font
-    font = pygame.font.SysFont('times new roman', 50)
+def game_logic():
+    """Handles the main game logic, including collisions and level updates."""
+    change_snake_direction()
+    modify_snake_pos()
+    boundary_check()
+    snake_body_check()
+    spawn_food()
 
-    game_over_screen = font.render("GAME OVER", True, "red")
 
-    game_over_rect = game_over_screen.get_rect()
+def spawn_food():
+    """Spawns food items on the screen based on the current level."""
+    global current_level
+    seconds = pygame.time.get_ticks() // 1000
+    if seconds % GAME_SPEED == 0 and seconds != current_level:
+        food_positions.clear()
+        num1, num2 = randint(1, 10), randint(1, 10)
+        global current_question
+        current_question = f"{num1} + {num2}"
+        food_positions.append(
+            [randint(10, WIDTH - 10), randint(10, HEIGHT - 10), num1 + num2])
+        for _ in range(3):
+            food_positions.append(
+                [randint(10, WIDTH - 10), randint(10, HEIGHT - 10), randint(0, 50)])
+        current_level = seconds
 
-    game_over_rect.midtop = (WIDTH//2, HEIGHT//2)
 
-    screen.blit(game_over_screen, game_over_rect)
+def reset():
+    global change_to
+    global direction
+    global snake_position
+    global snake_body
+    global current_level
+    global current_question
+    global food_positions
 
-    pygame.display.flip()
-
-    time.sleep(3)
-
-    exit_game()
+    direction = "right"
+    change_to = direction
+    current_level = 0
+    current_question = ""
+    snake_position = [100, 50]
+    snake_body = [[100, 50], [90, 50], [80, 50], [70, 50]]
+    food_positions = []
 
 
 def exit_game():
+    """Exits the game."""
     pygame.quit()
     sys.exit()
 
@@ -102,97 +186,49 @@ def main():
     global direction
     global snake_position
     global snake_body
+    global current_level
+    global current_question
 
-    current_level = 0
-    current_question = ""
+    # reset for new game
+    reset()
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit_game()
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_UP:
+                if event.key == pygame.K_UP or event.key == pygame.K_w:
                     change_to = 'up'
-                if event.key == pygame.K_DOWN:
+                elif event.key == pygame.K_DOWN or event.key == pygame.K_s:
                     change_to = 'down'
-                if event.key == pygame.K_LEFT:
+                elif event.key == pygame.K_LEFT or event.key == pygame.K_a:
                     change_to = 'left'
-                if event.key == pygame.K_RIGHT:
+                elif event.key == pygame.K_RIGHT or event.key == pygame.K_d:
                     change_to = 'right'
 
-        change_snake_direction()
-
-        modify_snake_pos()
-
-        screen.fill("black")
-
-        seconds = pygame.time.get_ticks() // 1000
-
-        if seconds != 0 and seconds % GAME_SPEED == 0:
-            if seconds != current_level:
-                # clear
-                food_positions.clear()
-
-                num1 = randint(1, 10)
-                num2 = randint(1, 10)
-
-                current_question = f"{num1} + {num2}"  # 5  + 5
-
-                # right answer
-                food_positions.append(
-                    [randint(10, WIDTH-10), randint(10, HEIGHT-10), num1 + num2])  # 10
-
-                # other wrong answers
-                for _ in range(3):
-                    food_positions.append(
-                        [randint(10, WIDTH-10), randint(10, HEIGHT-10), randint(0, 50)])
-
-                current_level = seconds
-
-        # add snake length
+         # add snake length
         snake_body.insert(0, list(snake_position))
 
         # remove snake length
         snake_body.pop()
 
-        # TODO: add instruction screen
+        # load game logic
+        game_logic()
 
-        boundary_check()
+        # set background
+        screen.fill("black")
 
-        snake_body_check()
+        # show question
+        draw_question()
 
-        # Spawn question
-        question_text = pygame.font.SysFont(
-            "time new roman", 50).render(current_question, True, "white")
+        # show answers
+        draw_food()
 
-        question_text_object = question_text.get_rect(
-            center=(WIDTH // 10, HEIGHT // 14))
-
-        screen.blit(question_text, question_text_object)
-
-        # Spawn food
-        for food in food_positions:
-            answer_object = pygame.draw.rect(screen, "purple",
-                                             pygame.Rect(food[0], food[1], 30, 30))
-            answer_text = pygame.font.SysFont("times new roman", 20).render(
-                f"{food[2]}", True, "black")
-            answer_text_object = answer_text.get_rect(
-                center=answer_object.center)
-
-            screen.blit(answer_text, answer_text_object)
-
-        # Spawn snake
-        for index, pos in enumerate(snake_body):
-            if index == 0:
-                pygame.draw.circle(
-                    screen, "red", (pos[0] * SNAKE_GAP + SNAKE_SIZE // 2, pos[1] * SNAKE_GAP + SNAKE_SIZE // 2), SNAKE_SIZE // 2)
-            else:
-                pygame.draw.rect(screen, SNAKE_COLOR,
-                                 pygame.Rect(pos[0] * SNAKE_GAP, pos[1] * SNAKE_GAP, SNAKE_SIZE, SNAKE_SIZE))
+        # show snake
+        draw_snake()
 
         pygame.display.update()
 
-        # TODO: make game speed faster by score?
         fps.tick(GAME_SPEED)
 
 
